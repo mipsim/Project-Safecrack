1b17e716727928ec8ddac64e00585de1d884f58a
mrzebrasmoothie <averyweibel@yahoo.com>
2022-01-10 22:12:14 -0800

    Add main menu functionality

--- Assets/Scripts/MainMenuBehavior.cs
+++ Assets/Scripts/MainMenuBehavior.cs
@@ -1,18 +1,46 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MainMenuBehavior : MonoBehaviour
 {
+    public static MainMenuBehavior instance;
+
+    public GameObject TutorialPage;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        if (instance != this) {
+            instance = this;
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    public void GoToGameplay() {
+        SceneManager.LoadScene(0);
+    }
+
+    public void GoToCredits()
     {
-        
+
+    }
+
+    public void OpenTutorial()
+    {
+        TutorialPage.SetActive(true);
+    }
+
+    public void CloseTutorial()
+    {
+        TutorialPage.SetActive(false);
+    }
+
+    public void ExitGame() { 
+        Application.Quit();
+    }
+
+    public void PrintName(GameObject buttonObj) {
+        Debug.Log(buttonObj.name);
     }
 }
